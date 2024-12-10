dc291191c2554e955fac41615fd581ba1e5200e2
anushkasj05 <anushka.sj05@gmail.com>
2024-12-10 17:49:11 +0530

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,73 +1,12 @@
-// wap using stack to find the min or max value without vactor
-#include <iostream>
-#include <stack>
-// #include<vector>
+#include<iostream>
 using namespace std;
-// int min(stack<int>s){
-//     vector<int>v;
-//     while(!s.empty()){
-//         v.push_back(s.top());
-//         s.pop();
-//     }
-//     int min = v[0];
-//     for(int i=1;i<v.size();i++){
-//         if(v[i]<min){
-//             min = v[i];
-//         }
-//     }
-//     return min;
-// }
-// int max(stack<int>s){
-//     vector<int>v;
-//     while(!s.empty()){
-//         v.push_back(s.top());
-//         s.pop();
-//     }
-//     int max = v[0];
-//     for(int i=1;i<v.size();i++){
-//         if(v[i]>max){
-//             max = v[i];
-//         }
-//     }
-//     return max;
-// }
-// int main(){
-//     stack<int>s;
-//     s.push(5);
-//     s.push(12);
-//     s.push(10);
-//     cout<<"Min value is "<<min(s)<<endl;
-//     cout<<"Max value is "<<max(s)<<endl;
-//     return 0;
-// }
-#include <iostream>
-#include <stack>
-using namespace std;
-int min(stack<int>s){
-    int min = s.top();
-    while(!s.empty()){
-        if(s.top()<min){
-            min = s.top();
-        }
-        s.pop();
+class node{
+    int data;
+    class node *l;
+    class node *r;
+    public:node(int a){
+        data=a;
+        l=NULL;
+        r=NULL;
     }
-    return min;
-}
-int max(stack<int>s){
-    int max = s.top();
-    while(!s.empty()){
-        if(s.top()>max){
-            max = s.top();
-        }
-        s.pop();
-    }
-    return max;
-}
-int main(){
-    stack<int>s;
-    s.push(5);
-    s.push(12);
-    s.push(10);
-    cout<<"Min value is "<<min(s)<<endl;
-    cout<<"Max value is "<<max(s)<<endl;
-}
+};
